0dcf3283332d8750a8051d1a91edc17b7a00a6c8
faizdifak <faizkurniawan297@gmail.com>
2025-08-28 20:38:52 +0700

    FDK Project1 Menampilkan Biodata diri gunakan Netbeans
    Pertemuan 1 PBO

---
+++ FDKproject1/src/main/java/com/mycompany/fdkproject1/FDKproject1.java
@@ -0,0 +1,14 @@
+
+
+package com.mycompany.fdkproject1;
+
+
+public class FDKproject1 {
+
+    public static void main(String[] args) {
+        System.out.println("Nama    : Faiz Difa Kurniawan");
+        System.out.println("NPM     : 2420506022");
+        System.out.println("Alamat  : Kebumen, Jawa Tengah");
+        System.out.println("No_HP   : 083144667083");
+    }
+}
